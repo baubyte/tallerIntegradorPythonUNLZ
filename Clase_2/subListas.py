0f2c2252c522f9e3da98846bf0d5fe1b5cc0cde2
baubyte <paredbaez.martin@gmail.com>
2021-04-06 13:42:03 -0300

    Clase 2 Rebanar Listas

--- Clase_2/subListas.py
+++ Clase_2/subListas.py
@@ -2,3 +2,23 @@
 lista = [1,2,3,4,5,6]
 subLista = lista[:3]
 print(subLista)
+#Reemplazar
+letras = ['a','a','a','a']
+print(letras)
+letras[1:3] = ['b']
+print(letras)
+#Vaciar Elementos
+letras1 = ['a','a','a','a']
+print(letras1)
+letras1[1:3] = []
+print(letras1)
+#Vaciar Lista
+letras2 = ['a','a','a','a']
+print(letras2)
+letras2[:] = []
+print(letras2)
+#Vaciar Lista
+letras3 = ['a','a','a','a']
+print(letras3)
+letras3 = []
+print(letras3)
