c82225adb616b98717af952a7112338b411bf7bc
baubyte <paredbaez.martin@gmail.com>
2021-04-06 13:27:53 -0300

    Clase Cadenas de Caracteres

---
+++ Clase_2/cadenasCracteres.py
@@ -0,0 +1,21 @@
+#Cadenas de Caracteres
+#Escapar un carácter \
+print('\"Hola Mundo\"')
+#Raw data
+print(r'\"Hola Mundo\"')
+#Concatenar
+print('\"Hola ' + 'Mundo\"')
+print('\"Hola '  'Mundo\"')
+#Salto de Linea
+print('\"Hola\"\n' + '\"Mundo\"')
+#Indexar cadenas de Texto
+nombre = 'BAUBYTE'
+print(nombre[0])
+print(nombre[-1])
+print(nombre[3:7])
+#Cortar cadenas de Texto
+palabra = 'Taller Integrador'
+palabra1 = palabra[7:12]
+palabra2 = palabra[0:3]
+print(palabra1)
+print(palabra2)
