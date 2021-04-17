cc875152a237b8b226247a46677f566f4f546318
baubyte <paredbaez.martin@gmail.com>
2021-04-17 13:12:02 -0300

    Ejercicio 1.3

--- GuiaEjercicios/Ejercicio1.3.py
+++ GuiaEjercicios/Ejercicio1.3.py
@@ -3,22 +3,29 @@ Ejercicio 1.3
 Pedir al usuario que ingrese por teclado dos números reales y 
 utilizarlos para realizar todas las operaciones aritméticas vistas 
 (suma, resta, multiplicación, división, potencia, división entera y resto).
-Mostar todos los resultados por pantalla (un resultado por línea) con 
+Mostrar todos los resultados por pantalla (un resultado por línea) con 
 su respectiva leyenda aclarando de que operación se trata.
  """
 #Variable
 print("**** Operaciones Aritméticas *****")
 print("***** Se aplicaran diversas operaciones a los números. ******")
-numeroUno = float(input("Ingrese un Número: "))
-numeroDos = float(input("Ingrese un Número: "))
+numeroUno = int(input("Ingrese un Número: "))
+numeroDos = int(input("Ingrese un Número: "))
 
 if numeroUno != 0 or numeroDos != 0:
     resultadoSuma = numeroUno + numeroDos
     resultadoResta = numeroUno - numeroDos
-    resultadoMuti = numeroUno * numeroDos
-    resultadoDiv = numeroUno / numeroDos if numeroUno !=0 and numeroDos != 0 else 0
-    print("Resultado de Sumar el Primer Numero mas el Segundo Numero Ingresado: ", resultadoSuma)
-    print("Resultado de Restar el Primer Numero menos el Segundo Numero Ingresado: ", resultadoResta)
-     print("Resultado de Multiplicar el Primer Numero por el Segundo Numero Ingresado: ", resultadoMuti)
+    resultadoMutiplicacion = numeroUno * numeroDos
+    resultadoDivEntera = numeroUno // numeroDos if numeroDos != 0 else "No se pude dividir por 0"
+    resultadoDivDecimal = numeroUno / numeroDos if numeroDos != 0 else "No se pude dividir por 0"
+    resultadoResto = numeroUno % numeroDos if numeroDos != 0 else "No se pude dividir por 0"
+    resultadoPotencia = numeroUno ** numeroDos
+    print("Resultado de Sumar ", numeroUno ," mas ", numeroDos ," es igual a: ", resultadoSuma)
+    print("Resultado de Restar ", numeroUno ," menos ", numeroDos ," es igual a: ", resultadoResta)
+    print("Resultado de Multiplicar ", numeroUno ," por ", numeroDos ," es igual a: ", resultadoMutiplicacion)
+    print("Resultado de elevar", numeroUno ," por ", numeroDos ," es igual a: ", resultadoPotencia)
+    print("Resultado de la Division Entera entre", numeroUno ," y ", numeroDos ," es igual a: ", resultadoDivEntera)
+    print("Resultado de la Division entre", numeroUno ," y ", numeroDos ," es igual a: ", resultadoDivDecimal)
+    print("El resto de la Division entre", numeroUno ," y ", numeroDos ," es igual a: ", resultadoResto)
 else:
-    print("¡Ups! Al Menos un Número ser Mayor a Cero\n para Realizar el Calculo")
+    print("¡Ups! Al Menos un Número debe ser Distinto a Cero\n para Realizar el Calculo")
