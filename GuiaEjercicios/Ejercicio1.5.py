0468fff165de86862d68714a03fbebca24a72550
baubyte <paredbaez.martin@gmail.com>
2021-04-17 14:11:57 -0300

    Ejercicio 1.5

---
+++ GuiaEjercicios/Ejercicio1.5.py
@@ -0,0 +1,11 @@
+"""
+Ejercicio 1.5
+Pedirle al usuario que ingrese cuatro números y mostrarle el promedio.
+ """
+#Variables
+numeroUno = int(input("Ingrese un Número: "))
+numeroDos = int(input("Ingrese un Número: "))
+numeroTres = int(input("Ingrese un Número: "))
+numeroCuatro = int(input("Ingrese un Número: "))
+promedio = (numeroUno + numeroDos + numeroTres + numeroCuatro) / 4
+print("El Promedio de lo Números Ingresados es:", promedio)
