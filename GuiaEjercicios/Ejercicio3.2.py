7b7a2222a729dc9376de3987969e3a16d6107067
baubyte <paredbaez.martin@gmail.com>
2021-04-20 16:43:54 -0300

    Ejercicio 3.2

---
+++ GuiaEjercicios/Ejercicio3.2.py
@@ -0,0 +1,22 @@
+"""
+Ejercicio 3.2
+Escribir una función para determinar si un número entero, ingresado por 
+teclado es un número primo.
+Un número primo es aquel que solo tiene como divisores enteros 
+(resto igual a cero) al número 1 y a sí mismo, por ejemplo, el número 5.
+"""
+
+def esPrimo(num):
+    for numero in range(2, num):
+        if num % numero == 0:
+            return False
+    return True
+
+print("***Determina si el Numero Ingresado es Primo***")
+
+numero = int(input("Ingrese un Número Mayor a Cero: "))
+
+if numero > 0:
+    print(f"El Numero {numero} es Primo") if esPrimo(numero) else print(f"El Numero {numero} no es Primo:")
+else:
+    print("El Numero Ingresado debe ser Mayor a Cero.")
