66b0007d4a30e105296a77305e3e24a5f85930fe
baubyte <paredbaez.martin@gmail.com>
2021-04-20 10:51:42 -0300

    Ejemplo Name Main

--- Ejemplos/importejemploNameMain.py
+++ Ejemplos/importejemploNameMain.py
@@ -1,1 +1,7 @@
+print("""
+    Ejecutando Desde otro archivo
+    Como no es el programa principal 
+    __name__ == "__main__" es false
+    por tanto, las funciones no se ejecutan
+    """)
 import ejemploNameMain
