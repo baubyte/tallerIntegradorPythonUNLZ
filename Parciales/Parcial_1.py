1986ccac073bb1247159451d2e23266fa59d468d
baubyte <paredbaez.martin@gmail.com>
2021-05-11 19:24:54 -0300

    Parcial

---
+++ Parciales/Parcial_1.py
@@ -0,0 +1,34 @@
+"""
+    Escribir un programa que verifique la validez de la CLAVE ingresadasiguiendo los 
+    siguentes criterios:
+        1.Debe contener una longitud mínima de 6 caracteres.
+        2.Debe tener una longitud máxima de 12 caracteres.
+        3.Debe contener al menos una letra entre [a-z]
+        4.Debe contener al menos una letra entre [A-Z]
+        5.Debe contener al menos un número [0-9]
+"""
+
+# Permite acceder a funcionalidades dependientes del Sistema Operativo
+import os
+# Permite utilizar expresiones regulares
+import re
+
+def validatePassword(password):
+    if 6 <= len(password) <= 12:
+        if re.search('[a-z]', password) and re.search('[A-Z]', password):
+            if re.search('[0-9]', password):
+                return True
+    return False
+
+
+usuario = input('Escriba su Usuario: ')
+while True:
+    os.system("clear")
+    print()
+    clave = input('Escriba la contraseña: ')
+    if validatePassword(clave):
+        print("Registro exitoso")
+        break
+    else:
+        print("La Contraseña no Cumple con los Parametros de Segurida")
+        input("Presione Enter e Intente de Nuevo")
