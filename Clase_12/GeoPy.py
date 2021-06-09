b43f7b29d523ecd37a150641220974eb4b6f324f
baubyte <paredbaez.martin@gmail.com>
2021-06-08 23:50:36 -0300

    geopy Clase 12

---
+++ Clase_12/GeoPy.py
@@ -0,0 +1,11 @@
+from geopy.geocoders import Nominatim
+
+geolocator = Nominatim(user_agent="baubyte")
+
+lista = ['Querandíes, 4451, Buenos Aires, Argentina',
+         'Gallo, 908, Buenos Aires, Argentina'
+        ]
+for index in lista:
+    location = geolocator.geocode(index, language="es")
+    print((location.latitude, location.longitude))
+    print(location.address)
