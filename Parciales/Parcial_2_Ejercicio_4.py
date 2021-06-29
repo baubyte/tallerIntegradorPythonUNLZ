6478c5ddf4b0009524b2fd3dc19b5ecf5efc6650
Juan P. Romano <programming@jpromano.net>
2021-06-29 19:27:33 -0300

    Update Parcial_2_Ejercicio_4.py

--- Parciales/Parcial_2_Ejercicio_4.py
+++ Parciales/Parcial_2_Ejercicio_4.py
@@ -4,11 +4,13 @@ class Numeros():
 
     def __init__(self):
         while True:
-            cantNumAzar = int(input('Ingrese la Cantidad de Números al Azar: '))
+            cantNumAzar = int(input('Ingrese la cantidad de números al azar: '))
+            minNum = int(input('Ingrese el inicio del rango o el numero minimo para arrancar: '))
+            maxNum = int(input('Ingrese el final del rango o el numero maximo para finalizar: '))
             if cantNumAzar < 0:
-                print("El Numero debe se mayor a cero.")
+                print("El Numero debe ser mayor a cero.")
             else:
-                self.valores = [random.randint(1,50) for _ in range(cantNumAzar)]
+                self.valores = [random.randint(minNum,maxNum) for _ in range(cantNumAzar)]
 
                 break
 
@@ -24,5 +26,9 @@ class Numeros():
 
 numeros = Numeros()
 
+print()
+print("Valores creados al azar: ")
 print(numeros.valores)
+print()
+print("Numero candidato de la lista: ")
 print(numeros.candidato())
